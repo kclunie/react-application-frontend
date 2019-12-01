5ca8475e3b864018ad40bd7becb1dc646445ee06
github username <github email address>
2019-11-30 20:26:49 -0500

    add tripInput form and addTrip action

--- src/components/TripInput.js
+++ src/components/TripInput.js
@@ -1,14 +1,39 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {addTrip} from '../actions/addTrip'
 
 class TripInput extends React.Component{
+
+    state = {
+        name: '',
+        date: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addTrip(this.state)
+    }
 
     render(){
         return (
             <div>
                 TripInput
+                <form onSubmit={this.handleSubmit}>
+                    <label>Trip Name:</label>
+                    <input type='text' placeholder='Name' value={this.state.name} name="name" onChange={this.handleChange}/><br/>
+                    <label>Date:</label>
+                    <input type='text' placeholder='Date' value={this.state.date} name="date" onChange={this.handleChange}/><br/>
+                    <input type="submit"/>
+                </form>
             </div>
         )
     }
 }
 
-export default TripInput
+export default connect(null, {addTrip})(TripInput)
