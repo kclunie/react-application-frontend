d87c15919c0e64e4b1a3694772e3c1789707200e
Kristen Clunie <kcluniesa@yahoo.com>
2020-03-18 21:29:09 -0400

    comment out TripInput2

--- src/components/TripInput2.js
+++ src/components/TripInput2.js
@@ -1,57 +1,57 @@
-import React from 'react'
-import { updateNewTripForm } from '../actions/addTrip2'
-import { addTrip } from '../actions/addTrip2'
-import {connect} from 'react-redux'
+// import React from 'react'
+// import { updateNewTripForm } from '../actions/addTrip2'
+// import { addTrip } from '../actions/addTrip2'
+// import {connect} from 'react-redux'
 
 
-const TripInput2 = ({formData, updateNewTripForm, addTrip, history}) => {
+// const TripInput2 = ({formData, updateNewTripForm, addTrip, history}) => {
 
-    const { name, date } = formData
-    console.log(name, date)
-    console.log(formData)
+//     const { name, date } = formData
+//     console.log(name, date)
+//     console.log(formData)
 
-    const handleChange = event => {
-        console.log("event.target in TripInput2 is", event.target)
-        const { name, value } = event.target
-        updateNewTripForm(name, value)
-    }
+//     const handleChange = event => {
+//         console.log("event.target in TripInput2 is", event.target)
+//         const { name, value } = event.target
+//         updateNewTripForm(name, value)
+//     }
 
-    const handleSubmit = event => {
-        console.log("formData in handleSubmit", formData)
-        console.log("event in handleSubmit", event)
-        event.preventDefault()
-        addTrip(formData)
-    }
+//     const handleSubmit = event => {
+//         console.log("formData in handleSubmit", formData)
+//         console.log("event in handleSubmit", event)
+//         event.preventDefault()
+//         addTrip(formData)
+//     }
 
-    return (
-        <div>
-        <form onSubmit={handleSubmit}>
-            <input
-            placeholder="name"
-            name="name"
-            onChange={handleChange}
-            value={name}
-            />
-             <input
-             placeholder="date"
-            name="date"
-            onChange={handleChange}
-            value={date}
-            />
-            <input
-            type="submit"
-            value="Create Trip"
-            />
-        </form>
-        </div>
-    )
-};
+//     return (
+//         <div>
+//         <form onSubmit={handleSubmit}>
+//             <input
+//             placeholder="name"
+//             name="name"
+//             onChange={handleChange}
+//             value={name}
+//             />
+//              <input
+//              placeholder="date"
+//             name="date"
+//             onChange={handleChange}
+//             value={date}
+//             />
+//             <input
+//             type="submit"
+//             value="Create Trip"
+//             />
+//         </form>
+//         </div>
+//     )
+// };
 
-const mapStateToProps = state => {
-    console.log("mapStateToProps in TripInput2 is", state)
-    return {
-     formData: state.tripReducer
-    }
-}
+// const mapStateToProps = state => {
+//     console.log("mapStateToProps in TripInput2 is", state)
+//     return {
+//      formData: state.tripReducer
+//     }
+// }
 
-export default connect(mapStateToProps, { updateNewTripForm, addTrip })(TripInput2)
+// export default connect(mapStateToProps, { updateNewTripForm, addTrip })(TripInput2)
