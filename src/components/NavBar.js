a551f1f3a20f736fd8cf417557f433e8329819e1
Kristen Clunie <kcluniesa@yahoo.com>
2020-03-20 20:56:32 -0400

    add NavBar container

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 const NavBar = () =>{
     return(
      <div className="NavBar">
+         <Container>
+         <Navbar expand="lg" bg="dark" variant="dark">
          WhereInTheWorld  |  
-         <NavLink to="/">Home  |  </NavLink>
+         <NavLink to="/" variant="light">Home  |  </NavLink>
          <NavLink to="/trips">My Trips  |  </NavLink>
          <NavLink to="/trips/new"> New Trip  |  </NavLink>
+         </Navbar>
+         </Container>
      </div>   
     )
 }
