3422e8ccdf77170ed59c99a26be049fae289152e
Kristen Clunie <kcluniesa@yahoo.com>
2020-03-24 16:05:00 -0400

    add niagara pic to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,16 +13,16 @@ const Home = () =>{
          <p className="HomeQuote">I've traveled far and near and want to keep my memories clear. <br></br>To remember exactly where I was and share my favorite things with the ones I love.</p>
         <br></br>
          <img className="Image" src="https://imgur.com/hKqv9uL.png" alt="lake"></img>
-         <img className="Image" src="https://imgur.com/a/CYfpsS1.png" alt="lake"></img>
+         <img className="Image" src="https://imgur.com/yEHvm4I.png" alt="niagara"></img>
    
          <Container></Container>
          <p></p>
          <Container>
             <Row>
-                <Col xs={1} md={6}>
+                <Col xs={3} md={3}>
                 {/* <Image src="holder.js/171x180" rounded /> */}
                 {/* <img className="Image" src="https://imgur.com/hKqv9uL.png" alt="lake"></img> */}
-                <Image className="Image" src="https://imgur.com/hKqv9uL.png" alt="lake" rounded/>
+                <Image className="Image" src="https://imgur.com/yEHvm4I.png" alt="lake" rounded/>
                 </Col>
                 <Col xs={6} md={4}>
                 <Image src="holder.js/171x180" roundedCircle />
@@ -35,16 +35,14 @@ const Home = () =>{
 
             <Figure>
                 <Figure.Image
-                    width={171}
-                    height={180}
+                    width={371}
+                    height={280}
                     alt="171x180"
-                    src="//imgur.com/a/CYfpsS1.png"
-                    // https://imgur.com/a/8plOIyu
-                    // <blockquote class="imgur-embed-pub" lang="en" data-id="a/8plOIyu"><a href="//imgur.com/a/8plOIyu"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
-                    // <blockquote class="imgur-embed-pub" lang="en" data-id="a/CYfpsS1"><a href="//imgur.com/a/CYfpsS1"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
+                    src="https://imgur.com/gYJf2Ae.png"
+                    // <blockquote class="imgur-embed-pub" lang="en" data-id="gYJf2Ae"><a href="//imgur.com/gYJf2Ae">View post on imgur.com</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
                 />
                 <Figure.Caption>
-                    Nulla vitae elit libero, a pharetra augue mollis interdum.
+                    Live with no excuses and travel with no regrets.
                 </Figure.Caption>
     </Figure>
      </div>   
