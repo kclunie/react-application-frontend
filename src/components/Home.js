f7434a78f9c9747a16369e72925fa098fee15f24
Kristen Clunie <kcluniesa@yahoo.com>
2020-04-10 19:20:22 -0400

    update hom page photos

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,8 +20,7 @@ const Home = () =>{
          <Container fluid>
             <Row className="ImageRow">
                 <Col>
-                    <Image className="ImageColumn" src="https://imgur.com/JxKSy0O.png" alt="lake" rounded/>
-                    
+                    <Image className="ImageColumn" src="https://imgur.com/JxKSy0O.png" alt="lake" rounded/>  
                 </Col>
                 <Col>
                     <Image className="ImageColumn" src="https://imgur.com/yEHvm4I.png" rounded/>
