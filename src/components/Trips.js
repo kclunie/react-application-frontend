81ccad278deefddbe2966b4723cb9bef35e7bee0
Kristen Clunie <kcluniesa@yahoo.com>
2020-06-03 13:56:01 -0400

    update titles to match all components

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -22,14 +22,13 @@ class Trips extends React.Component{
         <div>
 
             <br></br>
-            <h2 className="Title">Vacation Destination Scrap Book</h2>
+            <h1 className="Title">Vacation Destinations</h1>
             <input type="text" name="name" value={this.state.name} onChange={(event) => this.updateSearch(event)}  placeholder="Search"></input>
             <br></br><br></br>
             {filteredTrips.map(trip => 
             <li><Button variant="dark" key={trip.id}>
                 <Link className="TripList" to={`/trips/${trip.id}`}>{trip.name}<br></br></Link>
             </Button><br></br><br></br></li>)}
-           
 
         </div>
     )
