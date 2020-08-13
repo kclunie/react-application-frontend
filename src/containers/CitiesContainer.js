457b3badf268120fca01d66c9b874e5e38235f08
Kristen Clunie <kcluniesa@yahoo.com>
2020-08-12 21:37:23 -0400

    add likes component to CitiesContainer

--- src/containers/CitiesContainer.js
+++ src/containers/CitiesContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CityInput from '../components/CityInput'
 import Cities from '../components/Cities'
+import Likes from '../components/Likes'
 import City from '../components/City'
 
 class CitiesContainer extends React.Component{
@@ -10,6 +11,7 @@ class CitiesContainer extends React.Component{
             <div>
                 <Cities cities={this.props.trip && this.props.trip.cities} trip={this.props.trip}/>
                 <CityInput trip={this.props.trip}/>
+                
             </div>
         )
     }
