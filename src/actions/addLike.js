897f259053563cdf9f874192c9bbdf4552f37414
Kristen Clunie <kcluniesa@yahoo.com>
2020-08-12 21:39:05 -0400

    add like action

--- src/actions/addLike.js
+++ src/actions/addLike.js
@@ -1,14 +1,15 @@
-export const addLike = (city, tripId) => {
+export const addLike = (like, tripId) => {
 
     return (dispatch) => {
-        fetch(`http://localhost:3000/api/v1/trips/${tripId}/cities`, {
-            method: 'PATCH',
+        fetch(`http://localhost:3000/api/v1/trips/${tripId}/likes`, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(city)
+            body: JSON.stringify(like)
         })
         .then(response => response.json())
         .then(trip => dispatch({type: 'ADD_LIKE', payload: trip}))
     }
 }
+
