d6ec6f91fecfadcb6177b9897b70fd7c0ffd7a64
Kristen Clunie <kcluniesa@yahoo.com>
2020-08-25 22:27:30 -0400

    add likes form

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {addLike} from '../actions/addLike'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class Likes extends React.Component {
 
@@ -14,6 +18,7 @@ class Likes extends React.Component {
           count: newCount
         });
         this.props.addLike(this.state, this.props.trip.id)
+        console.log("like added", this.state)
       };
 
     render() {
@@ -21,7 +26,19 @@ class Likes extends React.Component {
           return (
           <div>
               <br></br>
-          <button onClick={this.addLikee}>Likes: {this.state.count} </button>
+          <button onClick={this.addLikee} name='count' value={this.state.count}>Likes: {this.state.count} </button>
+          <br></br>
+          {/* <form onSubmit={this.handleSubmit}>
+                    <Form.Group as={Row}>
+                    <Form.Label column sm="2">Location:</Form.Label>
+                    <Col sm="9">
+                    <Form.Control type='text' name='location' value={this.state.location} onChange={this.handleChange}></Form.Control> 
+                    </Col>
+                    </Form.Group>
+
+                    <Button type="submit" variant="primary">Submit</Button><br></br>
+
+                </form> */}
           </div> 
           )
       }
