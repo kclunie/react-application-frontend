b312b35ac60e3145a322aed7fb5f4c19df194de7
Kristen Clunie <kcluniesa@yahoo.com>
2020-11-27 22:27:24 -0500

    add heroku api url

--- src/actions/addLike.js
+++ src/actions/addLike.js
@@ -1,7 +1,8 @@
 export const addLike = (like, tripId) => {
     console.log(like, tripId)
     return (dispatch) => {
-        fetch(`http://localhost:3000/api/v1/trips/${tripId}/likes`, {
+        // fetch(`http://localhost:3000/api/v1/trips/${tripId}/likes`, {
+        fetch(`https://trips-backend.herokuapp.com/api/v1/trips/${tripId}/likes`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
