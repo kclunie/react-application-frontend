d1b2c294ea4403db970dcee3eb7f71268c3df993
Kristen Clunie <kcluniesa@yahoo.com>
2020-11-27 22:04:22 -0500

    update after adding static.json file

--- src/actions/addCity.js
+++ src/actions/addCity.js
@@ -1,5 +1,5 @@
 export const addCity = (city, tripId) => {
-
+    console.log(city, tripId)
     return (dispatch) => {
         fetch(`http://localhost:3000/api/v1/trips/${tripId}/cities`, {
             method: 'POST',
